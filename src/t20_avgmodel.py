5bf8227102524f900fa206d6dc0467e2c4525be0
adi1729 <adiyadav.1729@gmail.com>
2021-05-04 01:10:47 +0530

    first analysis

---
+++ src/t20_avgmodel.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  2 21:36:42 2021
+
+@author: aditya
+"""
+
+
+
+"""
+
+handle
+-- change in team name 
+-- change in venue name
+-- score at 7.1 rather than 5.6
+
+"""
+
+import pandas as pd
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = pd.read_csv(r'../Data/ipl_csv2/all_matches.csv')
+data.columns.tolist()
+
+data = data.fillna(0)
+data = data[data['innings'].isin([1,2])]
+data['scores'] = data['runs_off_bat']  + data['wides'] + data['noballs'] + data['byes']  + data['legbyes'] + data['penalty'] 
+
+data['venue']  = data['venue'].apply(lambda x : x.split(',')[0])
+
+# 
+
+np.sort(data['venue'].unique())
+np.sort(data['batting_team'].unique())
+#data['team_score'] = data.groupby(['match_id','innings'])['scores'].transform(lambda x : x.cumsum())
+
+venue_ipl2021 = {'Arun Jaitley Stadium': 'Feroz Shah Kotla',
+                 'Narendra Modi Stadium, Motera, Ahmedabad':'Sardar Patel Stadium',
+                 'M.Chinnaswamy Stadium' : 'M Chinnaswamy Stadium',
+                 'Rajiv Gandhi International Stadium, Uppal': 'Rajiv Gandhi International Stadium'
+          } 
+
+
+team_ipl2021 = {'Deccan Chargers': 'Sunrisers Hyderabad',
+                'Delhi Daredevils' :  'Delhi Capitals',
+                 'Rising Pune Supergiant' :  'Rising Pune Supergiants'
+                 } 
+
+data['venue'] = data['venue'].replace(venue_ipl2021, regex=True)
+data['batting_team'] =  data['batting_team'].replace(team_ipl2021, regex=True)
+data['bowling_team'] =  data['bowling_team'].replace(team_ipl2021, regex=True)
+
+
+venue= data[['venue','match_id']].drop_duplicates()['venue'].value_counts().reset_index()
+venue_list = venue[venue['venue']>10]['index']
+
+
+
+data = data[data['venue'].isin(venue_list)]
+
+
+tr = int(0.70*data['match_id'].unique().shape[0])
+tr_id = data['match_id'].unique()[0:tr]
+ts_id = data['match_id'].unique()[tr:819]
+
+train = data[data['match_id'].isin(tr_id)]
+test = data[data['match_id'].isin(ts_id)]
+
+train['team_score'] = train.groupby(['match_id','innings'])['scores'].transform(lambda x : x.cumsum())
+
+train = train[(train['ball']==5.6) & (train['innings'].isin(['1','2']))]
+
+venue_score  = train.groupby(['venue','innings','bowling_team'])['team_score'].mean().reset_index().rename(columns = {
+        'team_score':'pred_score'})
+     
+
+plt.figure(figsize=(10,5))
+g = sns.boxplot(x="venue", y="team_score", hue="innings", data=train)
+g.set_xticklabels( g.get_xticklabels(), rotation=90 , size = 7)
+# Show the plot
+plt.show()
+
+
+test = test.merge(venue_score , on =[ 'venue','innings','bowling_team'] ,how ='left')
+test['team_score'] =  test.groupby(['match_id','innings','bowling_team'])['scores'].transform(lambda x : x.cumsum())
+pred = test[(test['ball']==5.6) ][['match_id','innings','venue','batting_team','bowling_team','team_score','pred_score']]
+((pred['team_score'] - pred['pred_score'])**2/pred['team_score'].sum()).sum()
+
+
+"""
+strike rates -
+boundary/innings -
+batsman stike rate in first 15 balls - 
+bowler economy  -
+bowler strike rate in first 2 overs - 
+if new give - 60% of best 
+    
+venues - avg score in first 6 overs
+
+
+quanitfy attacking batting - batsman sr / match sr ?
+quantify wicket taking - bowlers sr / match sr ?
+quantify econmoical - bowler eco / match eco ?
+
+
+"""
+
+
+"""
+R squared
+-- only venues   3.3228891884282774 
+-- only venues and innings  3.329446773970389
+-- venues , innings and batting teams  3.199090642640181 -- can only leverage once we predict after toss
+-- venues, innings and bowling team 3.4677736945734274
+"""
+
+
+
+-- {batsman:
+    avg score in first 15 balls : s,
+    avg match scores : m,
+    avg bounday in first 15 balls : b,
+    sr in first 15 ballls :  sr
+    }
+    
+--  {bowler :
+    
+     }
+    
+data['striker_runs'] = 
+data['striker_balls'] =  
+data['striker_4s'] = 
+data['striker_6s'] = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
